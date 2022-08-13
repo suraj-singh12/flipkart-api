cd40d51e61647e264d203861d5c43a7d5db64885
suraj-singh12 <surajsingh5092@gmail.com>
2022-08-13 12:42:02 +0530

    updated supporting examples

--- app.js
+++ app.js
@@ -224,8 +224,8 @@ app.get('/filter/offers/:item', (req, res) => {
     // "discount": 75,
     // "delivery_type": "Free delivery",
     // "hidden_stars": 3.9,
-    // "item_id": 1
-    
+    // "item_id": 1,
+
     // "item_type": "mouses",
     // "name": "alpha1",
     // "email": "alpha1@alpha.com"
@@ -329,7 +329,16 @@ app.delete('/cart/deleteAll', (req, res) => {
 
 // add to wishlist
 // {
-//     "item_id": 58,
+//     "image": "https://i.ibb.co/HpkT5G0/5f5c2646fccf.jpg",
+//     "brand": "IDISI CLOTHES",
+//     "description": "Unstitched Polycotton Shirt Fabric Printed",
+//     "new_price": 249,
+//     "old_price": 999,
+//     "discount": 75,
+//     "delivery_type": "Free delivery",
+//     "hidden_stars": 3.9,
+//     "item_id": 1,
+
 //     "item_type": "mouses",
 //     "name": "alpha1",
 //     "email": "alpha1@alpha.com"
